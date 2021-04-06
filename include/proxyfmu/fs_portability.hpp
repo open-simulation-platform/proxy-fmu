9bf0483c405f5076cf789e7e2cb3a09fdce64ec7
Lars Ivar Hatledal <laht@ntnu.no>
2021-04-06 19:22:00 +0200

    fix conditional brackets

--- include/proxyfmu/fs_portability.hpp
+++ include/proxyfmu/fs_portability.hpp
@@ -2,19 +2,19 @@
 #ifndef PROXY_FMU_FS_PORTABILITY_HPP
 #define PROXY_FMU_FS_PORTABILITY_HPP
 
+
 #if __has_include(<filesystem>)
 #    include <filesystem>
 namespace proxyfmu
 {
-
 namespace filesystem = std::filesystem;
-
+}
 #else
 #    include <experimental/filesystem>
-
+namespace proxyfmu
+{
 namespace filesystem = std::experimental::filesystem;
-
-#endif
 }
+#endif
 
 #endif //PROXY_FMU_FS_PORTABILITY_HPP
