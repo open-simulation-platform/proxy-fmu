d4a4b74fa7615ffde3fe073e898964f85497a098
Lars Ivar Hatledal <laht@hials.no>
2021-04-16 00:41:10 +0200

    add test

---
+++ tests/test_identity.cpp
@@ -0,0 +1,83 @@
+#define BOOST_TEST_MODULE test_identity
+
+#include <proxyfmu/client/proxy_fmu.hpp>
+#include <proxyfmu/fmi/fmu.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+using namespace proxyfmu;
+using namespace proxyfmu::fmi;
+
+namespace
+{
+
+void test(fmu& fmu)
+{
+    auto slave = fmu.new_instance("instance");
+    BOOST_REQUIRE(slave->setup_experiment());
+    BOOST_REQUIRE(slave->enter_initialization_mode());
+    BOOST_REQUIRE(slave->exit_initialization_mode());
+
+    std::vector<value_ref> vr{0};
+
+    std::vector<double> realVal{0.0};
+    std::vector<int> integerVal{0};
+    std::vector<bool> booleanVal{false};
+    std::vector<std::string> stringVal{""};
+
+    std::vector<double> realRef(1);
+    std::vector<int> integerRef(1);
+    std::vector<bool> booleanRef(1);
+    std::vector<std::string> stringRef(1);
+
+    double t = 0.0;
+    double tEnd = 1.0;
+    double dt = 0.1;
+
+    while (t <= tEnd) {
+
+        slave->get_real(vr, realRef);
+        slave->get_integer(vr, integerRef);
+        slave->get_boolean(vr, booleanRef);
+        slave->get_string(vr, stringRef);
+
+        BOOST_TEST(realVal[0] == realRef[0]);
+        BOOST_TEST(integerVal[0] == integerRef[0]);
+        BOOST_TEST(booleanVal[0] == booleanRef[0]);
+        BOOST_TEST(stringVal[0] == stringRef[0]);
+
+        BOOST_REQUIRE(slave->step(t, dt));
+
+        realVal[0] += 1.0;
+        integerVal[0] += 1;
+        booleanVal[0] = !booleanVal[0];
+        stringVal[0] += 'a';
+
+        slave->set_real(vr, realVal);
+        slave->set_integer(vr, integerVal);
+        slave->set_boolean(vr, booleanVal);
+        slave->set_string(vr, stringVal);
+
+        t += dt;
+    }
+
+    BOOST_REQUIRE(slave->terminate());
+    slave->freeInstance();
+}
+
+} // namespace
+
+BOOST_AUTO_TEST_CASE(fmi_test_identity)
+{
+    std::string fmuPath("../fmus/1.0/identity.fmu");
+    auto fmu = loadFmu(fmuPath);
+    test(*fmu);
+}
+
+BOOST_AUTO_TEST_CASE(client_test_identity)
+{
+
+    std::string fmuPath("../fmus/1.0/identity.fmu");
+    auto fmu = client::proxy_fmu(fmuPath);
+    test(fmu);
+}
