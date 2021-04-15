1c70ab1d3988900b387baed2b35a94062bfe53e7
Lars Ivar Hatledal <laht@hials.no>
2021-04-15 11:14:32 +0200

    Update docs

--- cmake/FindFMILIB.cmake
+++ cmake/FindFMILIB.cmake
@@ -1,36 +1,11 @@
-# - CMake script for locating FMI Library
+# Find FMILIB
 #
-# This script is run by the command find_package(FMILibrary). The location of the
-# package may be explicitly specified using an environment variable named
-# FMILibrary_DIR or a CMake variable of the same name.  If both are specified, the
-# latter takes precedence.  The variable should point to the package
-# installation prefix, i.e. the directory that contains the "bin", "lib" and
-# "include" subdirectories.
+# Find the native FMILIB headers and libraries.
 #
-# The script searches for both static and shared/dynamic libraries, and creates
-# IMPORTED targets named "fmilib::static" and "fmilib::shared", respectively.  These
-# targets will have several of their IMPORTED_* and INTERFACE_* properties set,
-# making explicit use of commands like include_directories() superfluous in most
-# cases.
+#   FMILIB_INCLUDE_DIRS   - where to find fmilib.h
+#   FMILIB_LIBRARIES      - List of libraries when using FMILIB.
+#   FMILIB_FOUND          - True if FMILIB found.
 #
-# If the variable FMILibrary_USE_SHARED_LIB is set to TRUE, FMILibrary_LIBRARIES will
-# contain the name of the IMPORTED target "fmilib::shared". Otherwise, it will
-# contain "fmilib::static".
-#
-# After the script has completed, the variable FMILibrary_FOUND will contain whether
-# the package was found or not.  If it was, then the following variables will
-# also be set:
-#
-#    FMILibrary_INCLUDE_DIRS    - The directory that contains the header files.
-#    FMILibrary_LIBRARIES       - The name of an IMPORTED target.
-#
-#    FMILibrary_DLL             - Path to dynamic library (Windows only).
-#    FMILibrary_LIBRARY         - Path to static library.
-#    FMILibrary_SHARED_LIBRARY  - Path to shared/import library.
-#
-
-# Find static library, and use its path prefix to provide a HINTS option to the
-# other find_*() commands.
 find_path(FMILIB_INCLUDE_DIR NAMES fmilib.h)
 mark_as_advanced(FMILIB_INCLUDE_DIR)
 
