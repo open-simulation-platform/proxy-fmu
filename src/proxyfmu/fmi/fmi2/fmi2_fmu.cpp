3e9dfc56e937cc16d8ae1dff9013e957707b2720
Lars Ivar Hatledal <laht@hials.no>
2021-04-17 15:24:29 +0200

    Fix for fmi2 fmus supporting both me and cs

--- src/proxyfmu/fmi/fmi2/fmi2_fmu.cpp
+++ src/proxyfmu/fmi/fmi2/fmi2_fmu.cpp
@@ -17,7 +17,8 @@ fmi2_fmu::fmi2_fmu(std::shared_ptr<fmicontext> ctx, std::shared_ptr<temp_dir> tm
     , md_(create_model_description(handle_))
     , tmpDir_(std::move(tmpDir))
 {
-    if (fmi2_import_get_fmu_kind(handle_) != (fmi2_fmu_kind_cs | fmi2_fmu_kind_me_and_cs)) {
+    const auto kind = fmi2_import_get_fmu_kind(handle_);
+    if (!(kind == fmi2_fmu_kind_cs || kind == fmi2_fmu_kind_me_and_cs)) {
         throw std::runtime_error("FMU does not support Co-simulation!");
     }
 }
