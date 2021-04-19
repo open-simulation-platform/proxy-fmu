ebb097cfb24cad4f3eb9a1527dd76699eef2e444
Lars Ivar Hatledal <laht@hials.no>
2021-04-19 13:44:29 +0200

    test

--- cmake/FindTHRIFT.cmake
+++ cmake/FindTHRIFT.cmake
@@ -10,7 +10,7 @@
 find_path(THRIFT_INCLUDE_DIR NAMES thrift/Thrift.h)
 mark_as_advanced(THRIFT_INCLUDE_DIR)
 
-find_library(THRIFT_LIBRARY NAMES thrift thriftmd thriftmdd libthrift libthriftd)
+find_library(THRIFT_LIBRARY NAMES thrift thriftd thriftmd thriftmdd libthrift libthriftd)
 mark_as_advanced(THRIFT_LIBRARY)
 
 find_program(THRIFT_EXECUTABLE NAMES thrift)
