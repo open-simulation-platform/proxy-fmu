0146f7ce7fdf3579ad1bd762e517e6cbaf0e8f8e
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-07-21 09:08:13 +0200

    cleanup build (#71)

---
+++ tests/data_dir.hpp
@@ -0,0 +1,9 @@
+
+#ifndef PROXYFMU_DATA_DIR_HPP
+#define PROXYFMU_DATA_DIR_HPP
+
+#include <string>
+
+#define get_data(path) std::string(PROXYFMU_DATA_DIR) + "/"  + path;
+
+#endif // PROXYFMU_DATA_DIR_HPP
