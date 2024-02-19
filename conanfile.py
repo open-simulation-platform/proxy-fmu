748059f7c97cf7cb63c651f6bcbf39a2c8e5969f
Lars T. Kyllingstad <post-github@larskyllingstad.no>
2024-02-19 10:57:34 +0100

    Upgrade to Conan 2 (#92)
    * Attempt migration to conan version 2. Fails when finding Thrift executable as the provided FindTHRIFT.cmake is not used.
    
    * Update to Conan 2
    
    * Try a different way to get Conan in GitHub Actions
    
    * Actually install the intended GCC version
    
    * Set the CONAN_EXPORTED CMake variable again
    
    * Revert to using specific dependency versions
    
    * Try a different way of specifying dependencies
    
    * Adapt to Thrift dependency version constraints
    
    * Use Conan 1.x in the "Build" GH Actions workflow
    
    * Support recent Conan 1.x versions too
    
    * Remove support for running Conan from within CMake
    
    * Reinstate tests on CI, clean up workflows
    
    * Add OSP remote in build+test workflow
    
    * Fix toolchain path on Windows CI
    
    * Quote Windows path and try newer Linux distro
    
    * Github action update
    
    * Github action update
    
    * Uploading artifacts to jfrog
    
    gcc9 builds are not available from conancenter. Built libraries are uploaded to osp jfrog.
    
    ---------
    
    Co-authored-by: kristoffere <kristoffer.eide@sintef.no>
    Co-authored-by: David Heejong Park <hee.jong.park@dnv.com>
    Co-authored-by: David Heejong Park <88651540+davidhjp01@users.noreply.github.com>

--- conanfile.py
+++ conanfile.py
@@ -1,48 +1,62 @@
-from conans import ConanFile, CMake, tools
 from os import path
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
+from conan.tools.files import load
 
 
 class ProxyFmuConan(ConanFile):
     name = "proxyfmu"
+    def set_version(self):
+        self.version = load(self, path.join(self.recipe_folder, "version.txt")).strip()
+
     author = "osp"
     license = "MIT"
-    exports = "version.txt"
-    scm = {
-        "type": "git",
-        "url": "auto",
-        "revision": "auto"
-    }
+    url = "https://github.com/open-simulation-platform/proxy-fmu"
+
+    package_type = "library"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-    requires = (
-        "cli11/2.3.1",
-        "fmilibrary/2.3",
-        "thrift/0.13.0"
-    )
     options = {
-        "shared": [True, False]
+        "shared": [True, False],
+        "fPIC": [True, False]
     }
-    default_options = (
-        "shared=True"
-    )
+    default_options = {
+        "shared": True,
+        "fPIC": True
+    }
 
-    def set_version(self):
-        self.version = tools.load(path.join(self.recipe_folder, "version.txt")).strip()
+    generators = "CMakeDeps", "CMakeToolchain"
+    exports = "version.txt"
+    exports_sources = "*"
 
-    def configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
-        cmake.configure()
-        return cmake
+    def requirements(self):
+        self.tool_requires("cmake/[>=3.15]")
+        self.tool_requires("thrift/[~0.13]")
+        self.requires("boost/[~1.81]") # This version is required by Thrift
+        self.requires("cli11/[~2.3]")
+        self.requires("fmilibrary/[~2.3]")
+        self.requires("thrift/[~0.13]")
+        self.requires("zlib/1.2.13", override=True) # Also required by Thrift
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self.configure_cmake()
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["proxyfmu-client"]
-        self.cpp_info.defines = ["Boost_USE_STATIC_LIBS=ON"]
+        # Tell consumers to use "our" CMake package config file.
+        self.cpp_info.builddirs.append(".")
+        self.cpp_info.set_property("cmake_find_mode", "none")
