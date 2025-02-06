3a1d4450ed44ebd655aefa40b7fc7e4269d6d632
David Heejong Park <hee.jong.park@dnv.com>
2025-02-06 15:11:52 +0100

    Updated thrift

--- conanfile.py
+++ conanfile.py
@@ -31,12 +31,10 @@ class ProxyFmuConan(ConanFile):
 
     def requirements(self):
         self.tool_requires("cmake/[>=3.15]")
-        self.tool_requires("thrift/[~0.13]")
-        self.requires("boost/[~1.85]", transitive_headers=True, transitive_libs=True) # This version is required by Thrift
+        self.tool_requires("thrift/[~0.20]")
         self.requires("cli11/[~2.3]")
         self.requires("fmilibrary/[~2.3]")
         self.requires("thrift/[~0.20]", transitive_headers=True)
-        self.requires("zlib/1.2.13", override=True) # Also required by Thrift
 
     def config_options(self):
         if self.settings.os == "Windows":
