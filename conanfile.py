3137f37a6ceadb23059e8cb25993a2d593b74e88
David Heejong Park <hee.jong.park@dnv.com>
2025-02-06 14:53:56 +0100

    Updated boost to ~1.83

--- conanfile.py
+++ conanfile.py
@@ -32,7 +32,7 @@ class ProxyFmuConan(ConanFile):
     def requirements(self):
         self.tool_requires("cmake/[>=3.15]")
         self.tool_requires("thrift/[~0.13]")
-        self.requires("boost/[~1.81]") # This version is required by Thrift
+        self.requires("boost/[~1.83]") # This version is required by Thrift
         self.requires("cli11/[~2.3]")
         self.requires("fmilibrary/[~2.3]")
         self.requires("thrift/[~0.13]", transitive_headers=True)
