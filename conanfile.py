568ec1721bb74e4810a68636b522dbaee558fc6b
David Heejong Park <88651540+davidhjp01@users.noreply.github.com>
2026-09-15 09:17:24 +0200

    Fmilibrary upgrade to 3.0.4 (#106)
    * Update fmilibrary requirement to version 3.0.4
    
    * Update CI configuration to use GCC 14 and install necessary build tools
    
    * Add urlmon library dependency for FMILIB on Windows
    
    * Refactor CI configuration to use dynamic GCC image and add Shlwapi dependency for Windows
    
    * Update cli11 requirement to version 2.6
    
    * Remove unnecessary cleanup of apt lists in CI configuration
    
    * Remove unnecessary cleanup of apt lists in upload-conan-pkgs.yml

--- conanfile.py
+++ conanfile.py
@@ -34,7 +34,7 @@ class ProxyFmuConan(ConanFile):
         self.tool_requires("cmake/[>=4.0]")
         self.tool_requires("thrift/[~0.20]")
         self.requires("cli11/[~2.6]")
-        self.requires("fmilibrary/[~2.3]")
+        self.requires("fmilibrary/[~3.0.4]")
         self.requires("boost/[~1.85]") # Required by Thrift
         self.requires("thrift/[~0.20]", transitive_headers=True)
 
